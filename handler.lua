c4501605283f1010852c5d8c5175530507c6ffae
LunaCI <lunaci@luadist.org>
2018-06-08 01:06:52 +0000

    Update to version 0.2.0-1

--- handler.lua
+++ handler.lua
@@ -14,7 +14,99 @@ local NGX_ERR = ngx.ERR
 local NGX_DEBUG = ngx.DEBUG
 
 local RewriteHandler = BasePlugin:extend()
+destURL = {}
 
+-- checkMethod 判断请求方法是否合法
+local function checkMethod(config)
+
+    for _, value in ipairs(config.methods) do
+        ngx_log(NGX_ERR, value)
+        if value == ngx.req.get_method() then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- fillURL 填充URL地址
+-- 填充规则:
+-- 如果URL中存在 {},则视为取特定的Header。 如{X-Dest-URL},则表示取Header X-Dest-URL的值填充此段URL。当没有此Header时，则填充值为空
+--
+-- 获取填充值的规则:
+-- {}必须在同一个Pattern之中视为合法，反之非法。
+-- 例如:
+--      /{Header1}
+--      /{Header1}/
+--      均为合法。
+-- 而一下则视为非法：
+--      /{Header1/}
+--      {/Header1}
+--      {/Header1}/
+local function fillURL(url)
+    originURL = url
+    urls = utils.split(url, "/")
+    for idx, u in pairs(urls) do
+        if string.len(u) > 0 then
+            prefix = string.find(u, "{")
+            suffix = string.find(u, "}")
+            if prefix ~= nil and suffix ~= nil and prefix < suffix then
+                exHeader = string.sub(u, prefix + 1, suffix - 1)
+                if string.len(exHeader) > 0 then
+                    urls[idx] = req_get_headers()[exHeader]
+                else
+                    urls[idx] = ""
+                end
+            end
+        end
+    end
+
+    durl = ""
+    for idx, u in pairs(urls) do
+        if idx <=table.getn(urls)-1 then
+            durl = durl .. u .. "/"
+        else
+            durl = durl .. u
+        end
+
+    end
+
+    ngx_log(NGX_ERR, "FillURL Origin URL == [" .. originURL .. "] FillURL New URL == [" .. durl .. "]")
+    return durl
+end
+
+local function checkHeader(config)
+    for _, v in pairs(config.rules) do
+        rule = utils.split(v, "?", 2)
+        --[[正常情况不应该没有跳转URL,否则就失去了rewrite的意义]]
+        if table.getn(rule) == 1 then
+            return false
+        end
+
+        header = rule[1]
+
+        if req_get_headers()[header] then
+            destURL = utils.split(rule[2], "?", 2)
+            destURL[1] = fillURL(destURL[1])
+            ngx_log(NGX_ERR, destURL)
+            return true
+        else
+            return false
+        end
+    end
+end
+
+local function needRewrite(config)
+    if checkMethod(config) then
+        if checkHeader(config) then
+            return true
+        else
+            return false
+        end
+    else
+        return false
+    end
+end
 
 function RewriteHandler:new()
     RewriteHandler.super.new(self, "rewrite-plugin")
@@ -24,26 +116,26 @@ function RewriteHandler:access(config)
     RewriteHandler.super.access(self)
     log("RewriteHandler access")
     ngx_log(NGX_ERR, "===========================")
-    local url_args = ngx.req.get_uri_args()
-    for _, v in pairs(config.rules) do
-        rs = utils.split(v, "?", 2)
-        header = rs[1]
-        destURL = utils.split(rs[2], "?", 2)
-
+    if needRewrite(config) then
+        local url_args = ngx.req.get_uri_args()
         if table.getn(destURL) >= 2 then
             query = utils.split(destURL[2], "&")
             for _, value in pairs(query) do
                 tquery = utils.split(value, "=")
-                url_args[tquery[1]]=tquery[2]
+                url_args[tquery[1]] = tquery[2]
             end
         end
 
         if req_get_headers()[header] then
             ngx.var.upstream_uri = destURL[1]
         end
+        ngx.req.set_uri_args(url_args)
+    else
+        ngx_log(NGX_ERR, ngx.var.upstream_uri .. " NO MATCH REWRITE RULE Method[" .. ngx.req.get_method() .. "]")
     end
-    ngx.req.set_uri_args(url_args)
+
     ngx_log(NGX_DEBUG, require 'pl.pretty'.dump(ngx.var.query_string))
+    ngx_log(NGX_DEBUG, ngx.var.upstream_uri)
     ngx_log(NGX_ERR, "===========================")
 end
 
